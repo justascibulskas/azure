a53288c09d35f6445d19ba212e250f328d4ef00a
Justas Cibulskas <jucibulska@gmail.com>
2020-09-21 21:13:14 +0300

    initial commit

---
+++ bashshell-scripting/cibulskas.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#Name symbols
+echo $PWD
+echo "Enter your surname:"
+read SURNAME
+SURNAMELENGTH=${#SURNAME}
+echo "Your surname $SURNAME has $SURNAMELENGTH symbols."
+#NEW FOLDER
+echo "Creating folder name as : $SURNAME"
+mkdir ./$SURNAME
+echo "New folder created at:$PWD/$SURNAME"
+#COPY FILE
+echo "Copying file: passwd to foldernamed $SURNAME "
+cd/etc/passwd ~/$SURNAME
+echo "File passwd copied to $PWD/$SURNAME"
